6541200b2714c066e178f602e42e3faa0a2fbd47
Pranav Kolhe <pranavkolhe77@gmail.com>
2024-06-21 23:29:53 +0530

    Navbar created of project 1

---
+++ Project 1_Basic landing page/vite-project/src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import Navbar from './components/Navbar.jsx';
+
+
+function App() {
+
+  return (
+   <>
+    
+<Navbar />
+  </>
+  );
+}
+
+
+
+export default App;
---
+++ Project 1_Basic landing page/vite-project/src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+
+import React from "react"
+
+function Navbar() {
+    return(
+        <div>
+      <nav>
+        <div><img src="../public/brand_logo.png" alt="" srcset="" /></div>
+        <div>
+          <ul>
+            <li>MENU</li>
+            <li>LOCATION</li>
+            <li>ABOUT</li>
+            <li>CONTACT</li>
+          </ul>
+        </div>
+        <div><button type="button">Login</button></div>
+      </nav>
+    </div>
+    );
+}
+export default Navbar
